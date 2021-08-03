aac64753a53c556de47dbfe0e46855992ab26fc8
ThiagoCComelli <thiago.comelli@outlook.com>
2021-08-02 22:19:39 -0300

    manual annotations area

--- client/src/components/DashboardStats/DashboardStats.js
+++ client/src/components/DashboardStats/DashboardStats.js
@@ -5,6 +5,7 @@ import { getAccountStatus } from '../../utils/api/db'
 import ArrowForwardIcon from '@material-ui/icons/ArrowForward';
 import TrendingUpIcon from '@material-ui/icons/TrendingUp';
 import TrendingDownIcon from '@material-ui/icons/TrendingDown';
+import HighlightOffIcon from '@material-ui/icons/HighlightOff';
 import './DashboardStats.css'
 import { randomstring } from 'randomstring-js';
 
@@ -69,10 +70,101 @@ const ChartScreen = ({rawData,account}) => {
     )
 }
 
+const ManualControlNewItem = ({handleScreen}) => {
+    return (
+        <>
+            <div className="mainManualControlOptionsNewItem">
+                <input type="text" placeholder="Nome"></input>
+                <input type="number" placeholder="Valor"></input>
+                <div className="mainManualControlOptionsNewItemBttns">
+                    <button onClick={() => {handleScreen("ManualControlOptions")}}>Voltar</button>
+                    <button>Adicionar</button>
+                </div>
+            </div>
+        </>
+    )
+}
+
+const ManualControlOptions = ({handleScreen}) => {
+    return (
+        <>
+            <div className="manualControlOptions">
+                <span onClick={() => {handleScreen("ManualControlNewItem")}}>Novo item</span>
+                <span>Limpar items</span>
+            </div>
+        </>
+    )
+}
+
+const ManualControlContents = ({items}) => {
+    return (
+        <>
+        {items !== undefined ? items.map((item, index) => {
+            return (
+                <>
+                    <span>
+                        <HighlightOffIcon className="tagIcon" style={{cursor: "pointer",fontSize: 15, color: "#000"}}/>
+                        {item.nome} <strong>R${item.value}</strong>
+                    </span> {index === items.length-1 ? "=" : "+"}
+                </>
+            )
+        }) : null}
+        {<span style={{borderColor: "#4286F5"}}>Total <strong>R${items.reduce((a, b) => a + b.value, 0)}</strong></span>}
+        </>
+    )
+}
+
+const ManualControl = () => {
+    const [actualScreen, setActualScreen] = useState("ManualControlOptions")
+
+    const handleScreen = (screenName) => {
+        setActualScreen(screenName)
+    }
+
+    const [screen, setScreen] = useState({
+        "ManualControlOptions": <ManualControlOptions handleScreen={handleScreen}/>,
+        "ManualControlNewItem": <ManualControlNewItem handleScreen={handleScreen}/>
+    })
+
+    const items = [
+        {"nome":"iFood", "value": 15},
+        {"nome":"Warframe 4300pl", "value": 100},
+        {"nome":"Barbeiro", "value": 25},
+        {"nome":"Mercado", "value": 30},
+        {"nome":"Pizza", "value": 55},
+        {"nome":"Rodizio", "value": 50},
+        {"nome":"Bar", "value": 90},
+        {"nome":"Role dos brodi", "value": 45},
+        {"nome":"iFood", "value": 22},
+        {"nome":"Lavagem carro", "value": 43},
+        {"nome":"iFood", "value": 15},
+        {"nome":"Jogo Steam", "value": 65}
+    ]
+
+
+    return (
+        <>
+            <div className="mainManualControl">
+                <div className="mainManualControlOptions">
+                    <h3>Gastos - Anotações</h3>
+                    {screen[actualScreen]}
+                </div>
+                <div className="mainManualControlContents">
+                    <ManualControlContents items={items}/>
+                </div>
+            </div>
+        </>
+    )
+}
+
 function DashboardStats({account,handleNewItem}) {
     const [currentScreen, setCurrentScreen] = useState("Status")
     const [rawData,setRawData] = useState([])
-    const screens = {"Status": <ChartScreen rawData={rawData} account={account}/>, "NewItem": <NewItem account_id={account.id} handleNewItem={handleNewItem}/>}
+    const screens = {
+        "Status": <ChartScreen rawData={rawData} account={account}/>,
+        "NewItem": <NewItem account_id={account.id} handleNewItem={handleNewItem}/>,
+        "ManualControl": <ManualControl />
+    }
 
     const handleScreen = (screen) => {
         setCurrentScreen(screen)
@@ -96,7 +188,8 @@ function DashboardStats({account,handleNewItem}) {
     return (
         <div className="mainDashboardStats">
             <div className="mainDashboardStatsOptions">
-                <span onClick={() => {handleScreen("Status")}} className={`${currentScreen === "Status" ? "mainDashboardStatsOptionsActual" : null}`}>Status</span>
+                <span onClick={() => {handleScreen("Status")}} className={`${currentScreen === "Status" ? "mainDashboardStatsOptionsActual" : null}`}>Valores</span>
+                <span onClick={() => {handleScreen("ManualControl")}} className={`${currentScreen === "ManualControl" ? "mainDashboardStatsOptionsActual" : null}`}>Controle manual</span>
                 <span onClick={() => {handleScreen("NewItem")}} className={`${currentScreen === "NewItem" ? "mainDashboardStatsOptionsActual" : null}`}>Nova transacao</span>
             </div>
             <div className="mainDashboardStatsContent">
